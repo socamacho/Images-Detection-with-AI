d462f6fc1da776ad0979e30e201f526f903295de
socamacho <socamachoruiz@gmail.com>
2021-06-09 02:59:53 +0000

    Images detection

--- brain.py
+++ brain.py
@@ -9,6 +9,6 @@ prediction.setModelTypeAsMobileNetV2()
 prediction.setModelPath(os.path.join(execution_path, "mobilenet_v2.h5"))
 prediction.loadModel()
 
-predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "giraffe.jpg"), result_count=5 )
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "house.jpg"), result_count=5 )
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction , " : " , eachProbability)
